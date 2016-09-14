e1b315061401620b352ce12b7b97c48544f238da
Richard Yang <richard.yang@blackboard.com>
2016-09-14 17:38:10 +0800

    update:

---
+++ src/main/java/charpter1/section4/exercises/E2_StackPalindrome.java
@@ -0,0 +1,45 @@
+package charpter1.section4.exercises;
+
+import charpter1.section4.problems.P1_Palindrome;
+import org.junit.Test;
+
+import java.util.Stack;
+
+/**
+ * Created by riyang on 9/14/16.
+ */
+public class E2_StackPalindrome {
+
+    private P1_Palindrome p = new P1_Palindrome();
+
+    public boolean isPalindrome(Stack<Character> stack) {
+        char[] chars = new char[stack.size()];
+        int i = 0;
+        while (!stack.empty()) {
+            chars[i++] = stack.pop();
+        }
+
+        return p.isPalindrome(chars);
+    }
+
+    @Test
+    public void test() {
+        Stack<Character> stack = new Stack<Character>();
+        stack.push('m');
+        stack.push('a');
+        stack.push('d');
+        stack.push('a');
+        stack.push('m');
+
+        System.out.println(isPalindrome(stack));
+
+        stack = new Stack<Character>();
+        stack.push('m');
+        stack.push('a');
+        stack.push('d');
+        stack.push('a');
+        stack.push('b');
+
+        System.out.println(isPalindrome(stack));
+    }
+}
---
+++ src/main/java/charpter1/section6/problems/P1_AllPermutation.java
@@ -0,0 +1,55 @@
+package charpter1.section6.problems;
+
+import org.junit.Test;
+
+/**
+ * Created by riyang on 9/14/16.
+ */
+public class P1_AllPermutation {
+
+    public void calcAllPermutation(char[] chars) {
+        char[] xchars = new char[chars.length+1]; // 0 index ignored
+        System.arraycopy(chars, 0, xchars, 1, chars.length);
+        int length = xchars.length;
+        int base = length;
+
+        int min = 0;
+        for(int i = 1; i < length; ++i) {
+            min = i + min * base;
+        }
+
+        int max = 0;
+        for(int i = length-1; i > 0; --i) {
+            max = i + max * base;
+        }
+
+        for(int num = min; num <= max; ++num) {
+            int remaining = 0;
+            boolean illegal = false;
+
+            int num1 = num;
+            int[] bitmap = new int[length];
+            StringBuilder sb = new StringBuilder();
+            while(num1 > 0) {
+                remaining = num1 % base;
+
+                if(remaining == 0 || remaining > length || bitmap[remaining] == 1) {
+                    illegal = true;
+                    break;
+                }
+                sb.insert(0, xchars[remaining] + "\t");
+                bitmap[remaining] = 1;
+                num1 /= base;
+            }
+            if(illegal) continue;
+            System.out.print(sb.toString());
+            System.out.println(num);
+        }
+    }
+
+    @Test
+    public void test() {
+        char[] chars = {'a', 'b', 'c', 'd', 'e'};
+        calcAllPermutation(chars);
+    }
+}
