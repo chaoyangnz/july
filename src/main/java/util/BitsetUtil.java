c599d3e57cc4ed2049a280694ffd2f0fd4766015
Richard Yang <richdyang@users.noreply.github.com>
2017-01-03 12:43:53 +0800

    Update BitsetUtil.java

--- src/main/java/util/BitsetUtil.java
+++ src/main/java/util/BitsetUtil.java
@@ -10,6 +10,12 @@ public final class BitsetUtil {
         int offset = num % Integer.SIZE;
         bitmap[index] |= 1 << offset;
     }
+    
+    public static void clear(int[] bitmap, int num) {
+        int index = num / Integer.SIZE;
+        int offset = num % Integer.SIZE;
+        bitmap[index] &= ~(1 << offset);
+    }
 
     public static boolean get(int[] bitmap, int num) {
         int index = num / Integer.SIZE;
